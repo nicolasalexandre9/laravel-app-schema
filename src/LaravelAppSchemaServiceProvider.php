1c807b0d9d6d3ad8c90a0719ead40b0b7a79f586
Alexandre Nicolas <nicolasalexandre9@gmail.com>
2021-11-02 22:37:45 +0100

    feat(package): boilerplate empty package

---
+++ src/LaravelAppSchemaServiceProvider.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Nicolasalexandre9\LaravelAppSchema;
+
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * Class LaravelAppSchemaServiceProvider
+ *
+ * @category Laravel-app-schema
+ * @package  Laravel-app-schema
+ * @author   nicolas <nicolasalexandre9@gmail.com>
+ * @license  MIT https://opensource.org/licenses/MIT
+ */
+class LaravelAppSchemaServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
